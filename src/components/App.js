f207c7d2e81c1b637ef426b14fffa866345163ce
Sam <samturco42@gmail.com>
2023-05-09 15:19:32 -0700

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import items from "../data/items";
 
 function App() {
 
+  const [mode, setMode] = useState(false)
+
+  function handleClick() {
+    setMode((currentMode) => !currentMode)
+  }
+  
+  const appClass = mode ? "App dark" : "App light"
+
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+//! does line 22 mean if the mode is true then light? else dark? ...? why boolean?
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{mode ? 'Light' : 'Dark'} Mode</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [cartStatus, setCartStatus] = useState(false)
+  
+  function handleClick() {
+    setCartStatus(currentStatus => !currentStatus)
+  }
+
+  const appClass = cartStatus ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{cartStatus ? 'Remove from Cart' : 'Add to Cart'}</button>
     </li>
   );
 }
